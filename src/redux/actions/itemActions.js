9d71f50405c8a5aacff5b1452962dc4629f3b9cb
s-berntsson <susanna.berntsson2000@gmail.com>
2021-08-27 22:21:57 +0200

    removed old test files

--- src/redux/actions/itemActions.js
+++
@@ -1,5 +0,0 @@
-export function addItem(item) {
-    return { type: "ADD_ITEM", item: item };
-}  // actions = plain objects containing description of event
-    // must have a 'type'-property 
-    // pass simple data with it, not functions etc.
